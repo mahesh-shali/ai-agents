5c490eda387f179f5b552b8f0c4a46250cff8649
maheshshali25 <maiya9986@gmail.com>
2024-09-18 19:24:26 +0530

    first

---
+++ src/instagram/main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+import sys
+from instagram.crew import InstagramCrew
+import datetime
+
+
+def run():
+    # Replace with your inputs, it will automatically interpolate any tasks and agents information
+    inputs = {
+        'current_date': datetime.datetime.now().strftime("%Y-%m-%d"),
+        'instagram_description': input('Enter the page description here: '),
+        'topic_of_the_week': input('Enter the topic of the week here: '),
+    }
+    InstagramCrew().crew().kickoff(inputs=inputs)
+
+
+def train():
+    """
+    Train the crew for a given number of iterations.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        InstagramCrew().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while training the crew: {e}")
+
+def replay():
+    """
+    Replay the crew execution from a specific task.
+    """
+    try:
+        InstagramCrew().crew().replay(task_id=sys.argv[1])
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
+
+def test():
+    """
+    Test the crew execution and returns the results.
+    """
+    inputs = {
+        "topic": "AI LLMs"
+    }
+    try:
+        InstagramCrew().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
+
+    except Exception as e:
+        raise Exception(f"An error occurred while replaying the crew: {e}")
